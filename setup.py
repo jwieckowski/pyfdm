5e924649bd7136a970c86c23850662b02df0787a
Jakub Więckowski <kubaw1@onet.eu>
2025-08-04 13:56:34 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyfdm",
-    version="1.1.12",
+    version="1.1.13",
     author="Jakub Więckowski",
     author_email="jakub-wieckowski@zut.edu.pl",
     description="Python library for Fuzzy Decision Making",
